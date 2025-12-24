3eac2aacdfa8af743da96904d2cedac4257ae354
Lina Patil <2464051@cognizant.com>
2025-12-24 12:31:41 +0530

    Trending component done

--- TravelPackageManagementSystem/wwwroot/js/site.js
+++ TravelPackageManagementSystem/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.querySelectorAll('.card').forEach(card => {
 
-// Write your JavaScript code.
+    card.addEventListener('click', () => {
+
+        alert("Booking feature coming soon!");
+
+    });
+
+});
